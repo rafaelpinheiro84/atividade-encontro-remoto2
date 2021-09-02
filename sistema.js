77f82a3e6cb623c15b3ae2ba6ad47e28f803bc6a
rafaelpinheiro84 <rafaelsp0584@gmail.com>
2021-09-02 17:30:17 -0300

    sistema.js criado

---
+++ sistema.js
@@ -0,0 +1,40 @@
+let numeroDeAlunos = 10;
+
+let contador = 0;
+
+while (contador <= numeroDeAlunos) {
+       console.log(contador)
+    //   if(contador === 0){
+    //       console.log("O numero atual é ZERO")
+    //   } else if(contador % 2 == 1) {
+    //       console.log("O numero " + contador + " é IMPAR")
+    //   } else {
+    //       console.log("O numero " + contador + " é PAR")
+    //   }
+
+
+    ++contador;
+    // contador++;
+}
+
+// for (let contador = 1; contador <= numeroDeAlunos;contador++) {
+   
+//     if (contador === 0){
+//         console.log("O numero atual é zero");
+//     } else if (contador % 2 == 0){
+//         console.log("O numero " + contador + " é PAR")
+//     } else{
+//         console.log("O numero "  + contador + " é IMPAR")
+//     }
+// }
+
+
+// let listaDeAlunos = ["Rodrigo", "Yuri", "Guilherme", "Bianca", "Renata" ]
+
+// for (const aluno of listaDeAlunos){
+//      console.log(aluno);
+// }
+
+// listaDeAlunos.forEach(aluno => {
+//     console.log(aluno)
+// });
